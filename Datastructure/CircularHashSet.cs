b57f95bb2ac17a0bff85401b4a66efac10d415a2
Mustafa Kamal <mkamal@sejeltech.com>
2024-04-05 19:20:16 +0200

    Add project files.

---
+++ Datastructure/CircularHashSet.cs
@@ -0,0 +1,64 @@
+﻿namespace Datastructure
+{
+    public class CircularHashSet<T>
+    {
+        private readonly int _fixedSize;
+
+        public CircularHashSet(int fixedSize)
+        {
+            _fixedSize = fixedSize;
+            LinkedDictionary = new(_fixedSize);
+        }
+
+        private T OldKey = default;
+        private T PreviousKey = default;
+        private Dictionary<T, T?> LinkedDictionary;
+
+        public bool TryAdd(T key)
+        {
+            if (LinkedDictionary.Count < _fixedSize)
+            {
+                return TryAddInternal(key);
+            }
+
+            var nextOldKey = LinkedDictionary[OldKey];
+            LinkedDictionary.Remove(OldKey);
+            OldKey = nextOldKey;
+            return TryAddInternal(key);
+        }
+
+        public T? Find(T key)
+        {
+            LinkedDictionary.TryGetValue(key, out T? value);
+            return value;
+        }
+
+        public T[] GetKeys()
+        {
+            return LinkedDictionary.Keys.ToArray();
+
+        }
+
+        private bool TryAddInternal(T key)
+        {
+            bool isSuccess = LinkedDictionary.TryAdd(key, default);
+            if (!isSuccess)
+            {
+                return false;
+            }
+
+            if (LinkedDictionary.Count == 1)
+            {
+                OldKey = key;
+            }
+            else
+            {
+                PreviousKey = PreviousKey is null ? OldKey! : PreviousKey;
+                LinkedDictionary[PreviousKey!] = key;
+                PreviousKey = key;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Datastructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Datastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var circularSet = new CircularHashSet<string>(4);
+
+            for (int i = 0; i < 10; i++)
+            {
+                circularSet.TryAdd(i.ToString());
+                Console.WriteLine(i+" : "+ string.Join(", ",circularSet.GetKeys()));
+
+            }
+        }
+
+
+    }
+}
